3d7f72d6c9cf681264ee6c70262dea45ab30f12c
orilc <orilccc1o@gmail.com>
2024-03-10 19:38:52 +0200

    User Class

---
+++ app/src/main/java/com/example/todolistproject/User.java
@@ -0,0 +1,13 @@
+package com.example.todolistproject;
+
+public class User {
+    private String Email;
+    private String Password;
+    private String Name;
+
+    public User (String Email, String Password, String Name){
+        this.Email = Email;
+        this.Name = Name;
+        this.Password = Password;
+    }
+}
