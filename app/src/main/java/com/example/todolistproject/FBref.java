1c66468cc261cbc8627c1b4b55ddb98b214ed187
orilc <orilccc1o@gmail.com>
2024-03-10 20:29:51 +0200

    FBref

---
+++ app/src/main/java/com/example/todolistproject/FBref.java
@@ -0,0 +1,9 @@
+package com.example.todolistproject;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FBref {
+    public static FirebaseDatabase FBDB = FirebaseDatabase.getInstance();
+    public static DatabaseReference refUsers = FBDB.getReference("Users");
+}
